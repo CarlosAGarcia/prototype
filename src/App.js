46af1a0ed8c36c3919427be0607a98195b56b6a4
CPSCCAGE <32666769+CPSCCAGE@users.noreply.github.com>
2018-08-10 02:41:09 -0600

    Updated protoype site
    Fixed states, added table

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
 
 class custInfo extends Component {
@@ -17,41 +17,82 @@ class custInfo extends Component {
 }
 
 class FillForm extends Component {
-  /*styles = {
-    width: "50%",
-    display: "flex",
-    align-items: "center"
-
-  };*/
   state = {
-    headers: ["Firstname", "Lastname", "Email", "hello"],
-    item1: ["Firstname", "Lastname", "Email", "hello"],
-    item2: ["Firstname", "Lastname", "Email", "hello"],
-    item3: ["Firstname", "Lastname", "Email", "hello"]
+    headers: ["Product code", "Product Name", "Quantity", "Additional notes"],
+    item1: ["Firstname", "Lastname", "", "hello"],
+    item2: ["Firstname", "Lastname", "", "hello"],
+    item3: ["Firstname", "Lastname", "", "hello"]
   };
-  populateTable => ()
 
+  /* Needs to create constructor and super and bind 'this' 
+  populateTable() {
+    CODE
   }
+  */
+  populateTable = () => {
+    console.log("called");
+  };
+
+  isEditable = () => {
+    // const isQuantity = item == "" ? true : false;
+    return true;
+  };
+
   render() {
     return (
-      <div className="centered">
-        <table className="table table-bordered table-hover table-striped">
+      <div className="centered table-editable">
+        <table className="table table-bordered table-hover table-striped table-responsive-md">
           <thead>
             <tr>
               {this.state.headers.map(headers => (
-                <th key={headers}> {headers}</th>
+                <th key={headers} className="text-center">
+                  {" "}
+                  {headers}
+                </th>
               ))}
             </tr>
           </thead>
           <tbody>
             <tr>
-              {this.state.item1.map(item1 => <th key={item1}> {item1}</th>)}
+              {this.state.item1.map(item1 => (
+                <td
+                  key={item1}
+                  className="text-center "
+                  contentEditable={item1 === "" ? true : false}
+                >
+                  {" "}
+                  {item1}
+                </td>
+              ))}
             </tr>
             <tr>
-              {this.state.item2.map(item2 => <th key={item2}> {item2}</th>)}
+              {this.state.item2.map(item2 => (
+                <td
+                  key={item2}
+                  className="text-center"
+                  contentEditable={item2 === "" ? true : false}
+                >
+                  {" "}
+                  {item2}
+                </td>
+              ))}
             </tr>
             <tr>
-              {this.state.item3.map(item3 => <th key={item3}> {item3}</th>)}
+              {this.state.item3.map(item3 => (
+                <td
+                  key={item3}
+                  className="text-center"
+                  contentEditable={item3 === "" ? true : false}
+                >
+                  {" "}
+                  {item3}
+                </td>
+              ))}
+            </tr>
+            <tr>
+              {this.state.item1.map(item1 => {
+                this.populateTable();
+              })}
             </tr>
           </tbody>
         </table>
