1fdc3de1ffec09a6eddd8beb74b8b4140f4528f4
CPSCCAGE <32666769+CPSCCAGE@users.noreply.github.com>
2018-08-11 03:21:25 -0600

    Added 2 functions to create table
    creates Td and Tr dynamically based on information variable size in state

--- src/App.js
+++ src/App.js
@@ -17,11 +17,21 @@ class custInfo extends Component {
 }
 
 class FillForm extends Component {
+  constructor(props) {
+    super(props);
+    this.showStuff3 = this.createTDs.bind(this);
+    this.createTRs = this.createTRs.bind(this);
+  }
+
   state = {
     headers: ["Product code", "Product Name", "Quantity", "Additional notes"],
     item1: ["Firstname", "Lastname", "", "hello"],
     item2: ["Firstname", "Lastname", "", "hello"],
-    item3: ["Firstname", "Lastname", "", "hello"]
+    item3: ["Firstname", "Lastname", "", "hello"],
+    items: [
+      ["Firstname ", "Lastname ", "", "hello "],
+      ["Firstname1 ", "Lastname1 ", "", "hello1 "]
+    ]
   };
 
   /* Needs to create constructor and super and bind 'this' 
@@ -29,14 +39,58 @@ class FillForm extends Component {
     CODE
   }
   */
-  populateTable = () => {
-    console.log("called");
+  populateTable1 = item => {
+    console.log("2nd map recieved " + item);
+
+    item.map(item1 => console.log(item1));
+    return (
+      <tr>
+        <td>H</td>
+        <td>H</td>
+        <td>H</td>
+        <td>H</td>
+      </tr>
+    );
   };
 
-  isEditable = () => {
-    // const isQuantity = item == "" ? true : false;
-    return true;
+  showStuff = () => {
+    var array1 = [];
+    var k = this.createTDs(this.state.items);
+    return k;
   };
+
+  //gives all the columns in a given param
+  showStuff2 = () => {
+    console.log("SHOWSTUFF " + this.state.items);
+    console.log(this.state.items.length);
+    let cols = [];
+    for (var i = 0; i < this.state.items.length; i++) {
+      cols.push(<td key={i}>K</td>);
+    }
+
+    return <tr>{cols}</tr>;
+  };
+
+  //creates all the Rows in given item as <tr> and children as <td>
+  createTRs(items) {
+    var rows = [];
+    for (var i = 0; i < items.length; i++) {
+      rows.push(<tr key={i}>{this.createTDs(items[i])}</tr>);
+    }
+    var k = this.createTDs(items[0]);
+    return rows;
+  }
+  //gives all the columns in a given param as array of tds
+  createTDs(items) {
+    console.log("SHOWSTUFF " + items);
+    console.log(items.length);
+    let cols = [];
+    for (var i = 0; i < items.length; i++) {
+      cols.push(<td key={i}>{items[i]}</td>);
+    }
+
+    return cols;
+  }
 
   render() {
     return (
@@ -53,6 +107,31 @@ class FillForm extends Component {
             </tr>
           </thead>
           <tbody>
+            {this.state.items.map(item => {
+              console.log("FIRST MAP recieved" + item);
+              {
+                this.populateTable1(item);
+              }
+            })}
+
+            {this.createTRs(this.state.items)}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+class App extends Component {
+  render() {
+    return (
+      <div id="homePage">
+        <FillForm />
+      </div>
+    );
+  }
+}
+
+export default App; /*
             <tr>
               {this.state.item1.map(item1 => (
                 <td
@@ -88,26 +167,4 @@ class FillForm extends Component {
                   {item3}
                 </td>
               ))}
-            </tr>
-            <tr>
-              {this.state.item1.map(item1 => {
-                this.populateTable();
-              })}
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
-class App extends Component {
-  render() {
-    return (
-      <div id="homePage">
-        <FillForm />
-      </div>
-    );
-  }
-}
-
-export default App;
+            </tr>*/
